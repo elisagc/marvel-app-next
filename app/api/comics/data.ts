e20f2341d002195752fa8b479059c2d8fdbe9c10
Elisa García <elisagcubero@gmail.com>
2024-08-26 14:56:11 +0200

    fix: change env variables

--- app/api/comics/data.ts
+++ app/api/comics/data.ts
@@ -3,11 +3,12 @@ import { Comic, ComicApiResponse } from "@/types/Comics";
 export const getCharacterComics = async (id: string): Promise<Comic[]> => {
   const url = `https://gateway.marvel.com/v1/public/characters/${id}/comics?orderBy=issueNumber&`;
 
-  const urlApiKeyParams = `ts=${process.env.NEXT_PUBLIC_TIME_STAMP}&apikey=${process.env.NEXT_PUBLIC_MARVEL_API_KEY}&hash=${process.env.NEXT_PUBLIC_HASH_MD5}`;
-
+  const urlApiKeyParams = `ts=${process.env.TIME_STAMP}&apikey=${process.env.MARVEL_API_KEY}&hash=${process.env.HASH_MD5}`;
+  console.log(`${url}${urlApiKeyParams}`);
   const { data }: ComicApiResponse = await fetch(`${url}${urlApiKeyParams}`).then((res) => res.json());
 
-  return data.results.map((comic) => ({
+  if (!data?.results) return [];
+  return data?.results?.map((comic) => ({
     id: comic.id,
     image: `${comic.thumbnail.path}.${comic.thumbnail.extension}`,
     name: comic.title,
