66ed468046c0efa9ecf637cdfad7e1a4a0d32aba
Elisa García <elisagcubero@gmail.com>
2024-08-27 13:29:25 +0200

    feat: search controlled

--- components/search/Search.tsx
+++ components/search/Search.tsx
@@ -2,13 +2,21 @@
 
 import Image from "next/image";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import { useDebouncedCallback } from "use-debounce";
 import classes from "./Search.module.css";
+
 type SearchProps = React.InputHTMLAttributes<HTMLInputElement>;
+
 export const Search = (props: SearchProps) => {
   const searchParams = useSearchParams();
-
+  const search = searchParams.get("search") ?? "";
   const { replace } = useRouter();
+  const [searchTerm, setSearchTerm] = useState<string>(search);
+
+  useEffect(() => {
+    setSearchTerm(search);
+  }, [search]);
 
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
@@ -16,18 +24,15 @@ export const Search = (props: SearchProps) => {
     replace(`/?${params.toString()}`);
   }, 500);
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+    handleSearch(e.target.value);
+  };
+
   return (
     <div className={classes["search-container"]}>
       <Image src="/search.svg" alt="Search" width={16} height={16} className={classes.icon} />
-      <input
-        type="text"
-        className={classes.search}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-        defaultValue={searchParams.get("search")?.toString()}
-        {...props}
-      />
+      <input type="text" className={classes.search} value={searchTerm} onChange={handleChange} {...props} />
     </div>
   );
 };
